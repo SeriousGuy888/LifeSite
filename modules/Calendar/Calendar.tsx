f7d095302b7d84181b7076748642ca37fd2b211a
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 14:20:45 -0400

    calendar grid

--- modules/Calendar/Calendar.tsx
+++ modules/Calendar/Calendar.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react"
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
+    <section className="grid grid-cols-7">
+      {[...Array(31)].map((_, i) => {
+        return <p key={i}>{i + 1}</p>
+      })}
+    </section>
   )
 }
 
