7492733056ab5a35bf4cbef60ffe1c5de86edd06
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 15:14:28 -0400

    calendar displays month name

--- renderer/modules/Calendar/Calendar.tsx
+++ renderer/modules/Calendar/Calendar.tsx
@@ -2,12 +2,24 @@ import React from "react"
 import CalendarDay from "./CalendarDay"
 
 const Calendar = () => {
+  const [month, setMonth] = React.useState<Date>(new Date())
+
   return (
-    <section className="grid grid-cols-7 gap-2">
-      {[...Array(31)].map((_, i) => (
-        <CalendarDay key={i} day={i + 1} />
-      ))}
-    </section>
+    <div className="grid grid-cols-1 gap-4">
+      <section>
+        <h2 className="text-2xl">
+          {month.toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+          })}
+        </h2>
+      </section>
+      <section className="grid grid-cols-7 gap-2">
+        {[...Array(31)].map((_, i) => (
+          <CalendarDay key={i} day={i + 1} />
+        ))}
+      </section>
+    </div>
   )
 }
 
