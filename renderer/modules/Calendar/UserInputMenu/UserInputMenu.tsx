91f8afddceab7602c31525136781076c81347912
AndyYang69420 <129629226+AndyYang69420@users.noreply.github.com>
2023-08-12 17:39:05 -0400

    actually ACTUALLY fix and saved response

--- renderer/modules/Calendar/UserInputMenu/UserInputMenu.tsx
+++ renderer/modules/Calendar/UserInputMenu/UserInputMenu.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const validResponses = [
   "physically healthy",
@@ -6,91 +6,60 @@ const validResponses = [
   "physically unhealthy",
   "mentally healthy",
   "mentally moderate",
-  "mentally unhealthy",
-]
+  "mentally unhealthy"
+];
 
 export default function UserInput() {
-<<<<<<< Updated upstream
-  const [name, setName] = useState("")
-  const [isValid, setIsValid] = useState(true)
-  const [savedResponse, setSavedResponse] = useState("")
-=======
   const [name, setName] = useState("");
   const [isValid, setIsValid] = useState(true);
   const [savedResponse, setSavedResponse] = useState("");
->>>>>>> Stashed changes
 
   const handleInputChange = (e) => {
-    const inputValue = e.target.value
-    setName(inputValue)
-    setIsValid(validResponses.includes(inputValue))
-  }
+    const inputValue = e.target.value;
+    setName(inputValue);
+    setIsValid(validResponses.includes(inputValue));
+  };
 
   const saveResponse = () => {
     if (validResponses.includes(name)) {
-<<<<<<< Updated upstream
-      setSavedResponse(name)
-    }
-  }
-
-  let responseMessage = ""
-  let healthTip = ""
-=======
       setSavedResponse(name);
     }
   };
 
   let responseMessage = "";
   let healthTip = "";
->>>>>>> Stashed changes
 
   if (!isValid) {
-    responseMessage = "Please enter a valid health state"
+    responseMessage = "Please enter a valid health state";
   } else if (validResponses.includes(name)) {
     switch (name) {
-<<<<<<< Updated upstream
       case "physically healthy":
-        responseMessage = "Great job on maintaining a healthy physical state!"
-        healthTip = "Consider visiting our health tips page for more advice."
-        break
+        responseMessage = "Great job on maintaining a healthy physical state!";
+        healthTip = "Consider visiting our health tips page for more advice.";
+        break;
       case "physically moderate":
-        responseMessage = "Moderation is key to a balanced physical state."
-        healthTip =
-          "For tips on maintaining a moderate physical state, check out our health tips page."
-        break
+        responseMessage = "Moderation is key to a balanced physical state.";
+        healthTip = "For tips on maintaining a moderate physical state, check out our health tips page.";
+        break;
       case "physically unhealthy":
-        responseMessage =
-          "Consider adopting healthier habits for your physical well-being."
-        healthTip =
-          "Looking for ways to improve your physical health? Our health tips page can help."
-        break
+        responseMessage = "Consider adopting healthier habits for your physical well-being.";
+        healthTip = "Looking for ways to improve your physical health? Our health tips page can help.";
+        break;
       case "mentally healthy":
-        responseMessage = "Your mental well-being is in a good place!"
-        healthTip =
-          "Continue nurturing your mental health. Our health tips page offers insights."
-        break
+        responseMessage = "Your mental well-being is in a good place!";
+        healthTip = "Continue nurturing your mental health. Our health tips page offers insights.";
+        break;
       case "mentally moderate":
-        responseMessage =
-          "Strive for balance in maintaining your mental health."
-        healthTip =
-          "Maintaining a balanced mental state is important. Get tips from our health tips page."
-        break
+        responseMessage = "Strive for balance in maintaining your mental health.";
+        healthTip = "Maintaining a balanced mental state is important. Get tips from our health tips page.";
+        break;
       case "mentally unhealthy":
-        responseMessage =
-          "Take steps to improve your mental well-being and seek support if needed."
-        healthTip =
-          "If you're struggling, don't hesitate to seek help. Our health tips page has resources."
-        break
-      default:
-        responseMessage = "You are in an undefined health state."
-        break
-=======
-      // ... (same as before)
-
+        responseMessage = "Take steps to improve your mental well-being and seek support if needed.";
+        healthTip = "If you're struggling, don't hesitate to seek help. Our health tips page has resources.";
+        break;
       default:
         responseMessage = "You are in an undefined health state.";
         break;
->>>>>>> Stashed changes
     }
   }
 
@@ -105,14 +74,6 @@ export default function UserInput() {
       <p>{responseMessage}</p>
       {healthTip && (
         <p>
-<<<<<<< Updated upstream
-          {healthTip} <a href="/health-tips-page">Visit our health tips page</a>
-        </p>
-      )}
-      {savedResponse && <p>Saved Response: {savedResponse}.</p>}
-    </>
-  )
-=======
           {healthTip}{" "}
           <a href="/health-tips-page">Visit our health tips page</a>
         </p>
@@ -124,5 +85,4 @@ export default function UserInput() {
       )}
     </>
   );
->>>>>>> Stashed changes
 }
