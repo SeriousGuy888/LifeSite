b3cfc398f4c350fc4780f7dc13013c8e87bfee78
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 14:25:14 -0400

    fix calendar day count

--- modules/Calendar/Calendar.tsx
+++ modules/Calendar/Calendar.tsx
@@ -5,7 +5,7 @@ const Calendar = () => {
   return (
     <section className="grid grid-cols-7 gap-2">
       {[...Array(31)].map((_, i) => (
-        <CalendarDay day={i} />
+        <CalendarDay key={i} day={i + 1} />
       ))}
     </section>
   )
