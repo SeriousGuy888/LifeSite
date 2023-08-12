5f5e2d3a2e58c182da9bf7c608dc0a82061f4bca
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 14:29:24 -0400

    calendar constrained with or somethign

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Calendar from "../modules/Calendar/Calendar"
 
 export default function Page() {
-  return <>
-    <Calendar />
-  </>
+  return (
+    <main className="flex justify-center">
+      <div className="max-w-6xl">
+        <Calendar />
+      </div>
+    </main>
+  )
 }
--- modules/Calendar/CalendarDay.tsx
+++ modules/Calendar/CalendarDay.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const CalendarDay: NextPage<{ day: number }> = ({ day }) => {
   return (
-    <div className="bg-gray-100 p-8 aspect-square">
+    <div className="bg-gray-100 p-4 aspect-square">
       <h2 className="text-xl">{day.toString()}</h2>
     </div>
   )
