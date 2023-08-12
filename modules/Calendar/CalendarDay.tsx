d3350da87195362c2d3b7e07e1423b42e827c66c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 14:30:57 -0400

    calendar clickable

--- modules/Calendar/CalendarDay.tsx
+++ modules/Calendar/CalendarDay.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import { NextPage } from "next"
 import React from "react"
 
 const CalendarDay: NextPage<{ day: number }> = ({ day }) => {
   return (
-    <div className="bg-gray-100 p-4 aspect-square">
+    <div
+      className="bg-gray-100 p-4 aspect-square cursor-pointer"
+      onClick={() => alert(`clicked on ${day}`)}
+    >
       <h2 className="text-xl">{day.toString()}</h2>
     </div>
   )
