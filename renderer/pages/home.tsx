9af7df5edb91c80b5035e5cb7d980333e6fdd3b4
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 15:35:39 -0400

    remove link to other page

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -14,11 +14,11 @@ function Home() {
           <Calendar />
         </div>
       </div>
-      <div className="mt-1 w-full flex-wrap flex justify-center">
+      {/* <div className="mt-1 w-full flex-wrap flex justify-center">
         <Link href="/next">
           <a className="btn-blue">Go to next page</a>
         </Link>
-      </div>
+      </div> */}
     </>
   )
 }
