788f3e08affe2af9b3983be062b7f4e6e2c8b1d2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-13 16:37:22 -0400

    Update state.ts

--- renderer/lib/state.ts
+++ renderer/lib/state.ts
@@ -1,11 +1,5 @@
 import { atom } from "jotai"
 
-interface DayData {
-  date: Date
-  mood: number
-  journal: string
-}
-
 const currDate = new Date()
 export const selectedDayAtom = atom<Date>(
   new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate()),
