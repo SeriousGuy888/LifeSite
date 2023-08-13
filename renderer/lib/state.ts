1e39fec288be09a97092451a3c12138318f171e7
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-12 21:29:50 -0400

    interface for day data

--- renderer/lib/state.ts
+++ renderer/lib/state.ts
@@ -1,5 +1,11 @@
 import { atom } from "jotai"
 
+interface DayData {
+  date: Date
+  mood: number
+  journal: string
+}
+
 const currDate = new Date()
 export const selectedDayAtom = atom<Date>(
   new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate()),
