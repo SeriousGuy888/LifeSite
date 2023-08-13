ab66a36608e873f6aa8411d080f7482b5de6153d
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-13 11:27:03 -0400

    made slider change mood meter value

--- renderer/modules/UIElements/Slider/Slider.tsx
+++ renderer/modules/UIElements/Slider/Slider.tsx
@@ -1,21 +1,30 @@
-import React from "react";
-import styles from "./Slider.module.scss";
+import React, { useState } from "react"
+import styles from "./Slider.module.scss"
 
-declare var moodMeter: Number;
+declare var moodMeter: number
 
 const Slider = () => {
+  const [sliderVal, setSliderVal] = useState(50)
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const num = parseInt(event.target.value)
+    setSliderVal(num)
+    moodMeter = num
+  }
+
   return (
     <div>
       <input
         type="range"
         className={`${styles.slider} h-4 w-full appearance-none rounded-full`}
         id="slider"
+        value={sliderVal}
+        onChange={handleChange}
         min={0}
         max={100}
       />
       <label htmlFor="slider">Sad                                           Ok                                         Happy</label>
     </div>
-  );
-};
+  )
+}
 
-export default Slider;
+export default Slider
